9b6d0a5ca48873bd7faf26b8b9c1f662e03fb9d1
Terry Cain <tcain@bink.com>
2021-07-26 13:06:10 +0100

    Added TF 1.0 metadata

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.69.0"
+      configuration_aliases = [ azurerm.core ]
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
     name = var.resource_group_name
     location = var.location
